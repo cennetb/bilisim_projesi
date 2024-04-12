cca840585de65596085da1236a5f75f1bc08160f
cennetb <119862319+cennetb@users.noreply.github.com>
2024-04-12 19:44:50 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+import cv2
+
+def image_show():
+    resim = cv2.imread("mal1.jpg")
+    cv2.imshow("KANSER ", resim)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+
+if __name__ == '__main__':
+    image_show()
