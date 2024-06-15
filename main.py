cb9131a4bd26244c1f8e1dc383097745d4542d97
cennetb <boylucennet51@gmail.com>
2024-06-15 21:12:39 +0300

    excele kayıt eklendi adaptive_tresh olmadı

--- main.py
+++ main.py
@@ -4,28 +4,24 @@ import pandas as pd
 import os
 from openpyxl import load_workbook
 
-
 def image_show(image, window_name):
     """Verilen görüntüyü belirtilen pencere adıyla ekranda gösterir."""
     cv2.imshow(window_name, image)
 
-
 def gri(image):
     """Görüntü üzerinde bir işlem yap ve yeni bir görüntü döndür."""
+    # Örnek işlem: Görüntüyü gri tonlamaya çevir
     return cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
 
 def iso(image):
     """Görüntüyü ikili hale getirme (Isodata thresholding)."""
     _, isodata_goruntu = cv2.threshold(image, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
     return isodata_goruntu
 
-
 def apply_median_filter(image, kernel_size):
     """Median filtreleme uygula."""
     filtered_image = cv2.medianBlur(image, kernel_size)
     return filtered_image
-
 
 def find_contours(image):
     """Görüntüdeki şekillerin sınırlarını bulur ve koordinatlarını döndürür."""
@@ -37,11 +33,9 @@ def find_contours(image):
     contours, _ = cv2.findContours(binary, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     return contours
 
-
 def draw_contours(image, contours):
     """Görüntüdeki şekillerin sınırlarını çiz ve ekranda göster."""
     cv2.drawContours(image, contours, -1, (0, 255, 0), 5)
-
 
 def draw_circles(image, contours):
     """Görüntüdeki şekillerin etrafına minimum çemberler çizer ve çaplarını hesaplar."""
@@ -56,7 +50,6 @@ def draw_circles(image, contours):
     print(f"Toplam {len(circle_diameters)} tane sınır bulundu.")
     return circle_diameters
 
-
 def create_mask(image):
     """Create a mask based on green contours in the image."""
     lower_green = np.array([0, 255, 0])
@@ -69,12 +62,10 @@ def create_mask(image):
     cv2.drawContours(mask_filled, contours, -1, (255), thickness=cv2.FILLED)
     return mask_filled
 
-
 def calculate_area(mask):
     """Calculate the area size of the masked region."""
     area_size = cv2.countNonZero(mask)
     return area_size
-
 
 def calculate_gray_stats(gray_image, mask):
     """Calculate mean, min, and max gray values within the mask."""
@@ -84,25 +75,25 @@ def calculate_gray_stats(gray_image, mask):
     max_gray = np.max(masked_gray[np.where(mask != 0)])
     return mean_gray, min_gray, max_gray
 
-
 def calculate_rgb_stats(image, mask):
     """Calculate mean RGB values within the mask."""
     masked_rgb = cv2.bitwise_and(image, image, mask=mask)
     mean_rgb = cv2.mean(masked_rgb, mask=mask)
     return mean_rgb
 
-
 if __name__ == '__main__':
-    # Görüntüyü yükle
-    image_path = "./dataset/train/train/skin_cancer/skin_cancer_10.jpg"
+
+    image_path = "./dataset/train/train/not_skin_cancer/not_skin_cancer_43.jpg"
+    #image_path = "./dataset/train/train/skin_cancer/skin_cancer_08.jpg"
     image_name = os.path.basename(image_path)
     origin = cv2.imread(image_path)
     image = cv2.imread(image_path)
     image = cv2.resize(image, None, fx=2, fy=2, interpolation=cv2.INTER_LINEAR)
     image1 = gri(image)
+    #image2 = cv2.adaptiveThreshold(image1,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,21,3)
     image2 = cv2.equalizeHist(image1)
-    image3 = iso(image1)
-    image4 = apply_median_filter(image3, 33)
+    image3 = iso(image2)
+    image4 = apply_median_filter(image3, 3)
     contours = find_contours(image4)
     draw_contours(image, contours)
     diameters = draw_circles(image, contours)
@@ -142,17 +133,15 @@ if __name__ == '__main__':
     df_new = pd.DataFrame(data)
     file_name = "gorsel_ozellikler.xlsx"
 
+    # Append new data to the existing Excel file
     if os.path.exists(file_name):
-        book = load_workbook(file_name)
-        writer = pd.ExcelWriter(file_name, engine='openpyxl')
-        writer.book = book
-        df_existing = pd.read_excel(file_name)
-        df_existing = df_existing[df_existing["Görsel Adı"] != image_name]
-        df_combined = pd.concat([df_existing, df_new], ignore_index=True)
-        with pd.ExcelWriter(file_name, engine='openpyxl') as writer:
-            df_combined.to_excel(writer, index=False, sheet_name='Sheet1')
+        with pd.ExcelWriter(file_name, engine='openpyxl', mode='a', if_sheet_exists='overlay') as writer:
+            df_new.to_excel(writer, index=False, header=False, startrow=writer.sheets['Sheet1'].max_row)
     else:
         df_new.to_excel(file_name, index=False)
+
+    df_new = pd.DataFrame(data)
+    file_name = "yeni_gorsel_ozellikler.xlsx"
 
     # Görüntüleri aynı anda açma
     images = [origin, image1, image2, image3, image, image4]
@@ -164,6 +153,6 @@ if __name__ == '__main__':
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
-# image2 = cv2.Laplacian(image, cv2.CV_64F)
+    # image2 = cv2.Laplacian(image, cv2.CV_64F)
     # image2 = cv2.convertScaleAbs(image2)
-   # image2 = unsharp_mask(image2, kernel_size=(5, 5), sigma=1.0, amount=1.5, threshold=10)
+    # image2 = unsharp_mask(image2, kernel_size=(5, 5), sigma=1.0, amount=1.5, threshold=10)
